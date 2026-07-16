f9af37aa88e1bccc69425eba98123f87adb5262e
austen.hernandez <austen.hernandez@ENGRBHCP5S3>
2026-07-16 17:37:19 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// This program will attempt to create a basic stack system from scratch
+// Austen Hernandez
+// 7.16.26
+
+using Build_Stack.StackFile;
+
+public class Program
+{
+    public static void Main()
+    {
+        SimpleStack actionHistory = new SimpleStack();
+        actionHistory.Push("Move Forward");
+        actionHistory.Push("Open Chest");
+        actionHistory.Push("Drink Potion");
+        Console.WriteLine($"{actionHistory.Pop()} undone.");
+        actionHistory.Peek();
+        actionHistory.PrintStack();
+    }
+}
---
+++ SimpleStack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Build_Stack.StackFile
+{
+    public class SimpleStack
+    {
+        private string[] items = new string[10];
+        private int count = 0;
+        public void Push(string input)
+        {
+            count++;
+            items[items.Length - count] = input;
+        }
+        public string Pop()
+        {
+            if (!IsEmpty())
+            {
+                int difference = items.Length - count;
+                string itemRemoved = items[difference];
+                items[difference] = null;
+                count--;
+                return itemRemoved;
+            }
+            else
+                return "No items in stack";
+        }
+        public void Peek()
+        {
+            int difference = items.Length - count;
+            string itemRemoved = items[difference];
+            Console.WriteLine(itemRemoved);
+        }
+        public bool IsEmpty()
+        {
+            bool empty = true;
+            if (count == 0)
+            {
+                return empty;
+            }
+            else
+                return !empty;
+        }
+        public void PrintStack()
+        {
+            foreach(string input in items.Reverse())
+            {
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
